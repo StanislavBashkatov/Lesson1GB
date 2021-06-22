14d6610d2e6273b83dc29019c742e920e09b01b6
StanislavAB <stas.bashckatov@yandex.ru>
2021-06-22 21:29:07 +0300

    Task 7

--- src/ru/geekbrains/taskMain.java
+++ src/ru/geekbrains/taskMain.java
@@ -7,6 +7,7 @@ public class taskMain {
         System.out.println("Hello World");
         System.out.println(number(12, 3));
         System.out.println(numberFull(-1));
+        hello("Anton");
         int a = 1;
         char b = 'b';
         byte c = 127;
@@ -24,8 +25,13 @@ public class taskMain {
         return (a + b >= 10 && a + b <= 20);
     }
 
-    public static boolean numberFull (int a){
-        return a>0;
+    public static boolean numberFull(int a) {
+        return a > 0;
+    }
+
+    public static void hello(String name) {
+        System.out.println("Hello " + name);
+
     }
 }
 
